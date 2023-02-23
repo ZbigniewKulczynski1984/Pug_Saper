8b2a19fd6ff1ab6c9ec2ec3f2327776b673cfa17
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-23 14:08:07 +0100

    modified

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const btn = document.querySelector('.search-btn')
+const input = document.querySelector('.search-input')
 
+const showSearchBar = () => {
+    input.classList.toggle('active')
+}
+
+btn.addEventListener('click', showSearchBar)
 open.addEventListener('click', () => container.classList.add ('show-nav'));
-
 close.addEventListener('click', () => container.classList.remove ('show-nav'));
