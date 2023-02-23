6eae768160458037d6438768ac83c5ae5054ba6b
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-23 15:42:47 +0100

    code added

--- js/slider.js
+++ js/slider.js
@@ -6,3 +6,30 @@ const carouselWith = 800
 const carouselSpeed = 3000
 
 let index = 0
+
+const handleCarousel = () => {
+    index++
+    changeImage()
+}
+
+const changeImage = () => {
+    if(index > carouselImages.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = carouselImages.length -1
+    }
+
+    slidesContainer.style.transform = `translateX(${- index * carouselWith}px)`
+}
+
+const handleRightArrow = () => {
+    index ++
+    changeImage()
+}
+
+const handleLeftArrow = () => {
+    index --
+    changeImage()
+}
+rightBtn.addEventListener('click', handleRightArrow)
+leftBtn.addEventListener('click', handleLeftArrow)
