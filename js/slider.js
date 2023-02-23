e7c1ce578a6f772334d1b710255cba3d7e30177a
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-23 15:28:27 +0100

    code added to slider.js

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+const slidesContainer = document.querySelector('.slides-container')
+const leftBtn = document.querySelector('.btn-left')
+const rightBtn = document.querySelector('.btn-right')
+const carouselImages = document.querySelectorAll('.slide')
+const carouselWith = 800
+const carouselSpeed = 3000
+
+let index = 0
